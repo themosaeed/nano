6bd328d0962247229827cec1708bdee7041685ee
themosaeed <mo.elsaeed.aly@gmail.com>
2022-11-28 14:56:18 +0200

    added2

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_HOST=database.caounmzo6jzy.us-west-2.rds.amazonaws.com
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=DU1g7Y3PmV4VuebhhhNU
+export TYPEORM_DATABASE=glee
+export TYPEORM_PORT=5432
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
---
+++ .circleci/ansible/roles/deploy/files/myenv.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_HOST=database-1.c2aoy6hdfiyr.us-east-1.rds.amazonaws.com
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=1GdV6PtEDqyahTDa6zqU
+export TYPEORM_DATABASE=glee
+export TYPEORM_PORT=5432
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*.ts
